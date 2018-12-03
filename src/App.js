3e76df7d1245a4bf00bf4e5b604ffbd2f8e06667
Tobias Norton <tnorton20@yahoo.com>
2018-12-03 14:12:33 -0500

    Movie posters displaying

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <main className="movies-area">
           <Film />
         </main>
-        <Profile />
+        {/* <Profile /> */}
       </div>
     )
   }
--- src/Film.js
+++ src/Film.js
@@ -3,11 +3,22 @@ import content from './content.json'
 
 class Film extends Component {
   render() {
+    let movieObjects = content.results
     return (
-      <div className="movie">
-        <img src="#" alt="Movie Title" />
-        <h3>Movie Title</h3>
-      </div>
+      <>
+        {movieObjects.map((movieObject, index) => {
+          return (
+            <div key={index} className="movie">
+              {console.log(movieObject.poster_path)}
+              <img
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${movieObject.poster_path}`}
+                alt="Movie Title"
+              />
+              <h3>{movieObject.title}</h3>
+            </div>
+          )
+        })}
+      </>
     )
   }
 }
